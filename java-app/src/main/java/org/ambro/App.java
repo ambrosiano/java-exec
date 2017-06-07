39abf1c16f62557cb9eb859d237e7f58934d61e9
John Ambrosiano <ambro@lanl.gov>
2017-06-07 10:30:51 -0600

    Added shapefile reading test.

--- java-app/src/main/java/org/ambro/App.java
+++ java-app/src/main/java/org/ambro/App.java
@@ -1,13 +1,14 @@
 package org.ambro;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.LineNumberReader;
 import java.net.URL;
 import java.util.Properties;
+
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureSource;
 
 /**
  * Hello world!
@@ -19,11 +20,20 @@ public class App
     {
     	// Read from the packaged properties file.
     	try {
-			InputStream istream = App.class.getResourceAsStream("/src/main/resources/config.txt");
+    		// Read a single file from an input stream.
+			InputStream istream = App.class.getResourceAsStream("/config.txt");
 			Properties props = new Properties();
 			props.load(istream);
 			String name = props.getProperty("name");
 	        System.out.println( "Hello, "+name+"!" );
+	        
+	        // Read a shapefile from a URL.
+	        URL url = App.class.getResource("/coastline/states.shp");
+			ShapefileDataStore inDataStore = new ShapefileDataStore(url);
+			SimpleFeatureSource inFeatureStore = (SimpleFeatureSource) inDataStore.getFeatureSource();
+			SimpleFeatureCollection inFeatureCollection = inFeatureStore.getFeatures();
+			System.out.println(inFeatureCollection.size()+" shapefile features read.");
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
