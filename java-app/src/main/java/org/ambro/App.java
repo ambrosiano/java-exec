8202862ca9127722e8d1981e55dc48173e6070b7
John Ambrosiano <ambro@lanl.gov>
2017-06-07 17:39:36 -0600

    Successful implementation of the test build and execution.

--- java-app/src/main/java/org/ambro/App.java
+++ java-app/src/main/java/org/ambro/App.java
@@ -4,11 +4,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Properties;
 
 import org.geotools.data.shapefile.ShapefileDataStore;
 import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
+import org.opengis.feature.simple.SimpleFeature;
+
+import com.vividsolutions.jts.geom.Geometry;
 
 /**
  * Hello world!
@@ -25,15 +31,16 @@ public class App
 			Properties props = new Properties();
 			props.load(istream);
 			String name = props.getProperty("name");
-	        System.out.println( "Hello, "+name+"!" );
+	        System.out.println( "Hello, "+name+"." );
 	        
 	        // Read a shapefile from a URL.
-	        URL url = App.class.getResource("/coastline/states.shp");
+	        URL url = App.class.getResource("/coastline/conus.shp");
 			ShapefileDataStore inDataStore = new ShapefileDataStore(url);
 			SimpleFeatureSource inFeatureStore = (SimpleFeatureSource) inDataStore.getFeatureSource();
 			SimpleFeatureCollection inFeatureCollection = inFeatureStore.getFeatures();
 			System.out.println(inFeatureCollection.size()+" shapefile features read.");
-			
+			Collection<Geometry> geometries = createGeometries(inDataStore);
+			System.out.println(geometries.size()+" geometries found.");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -41,4 +48,30 @@ public class App
 		}
     	
     }
+    
+	private static Collection<Geometry>  createGeometries(ShapefileDataStore inDataStore){
+		Collection<Geometry> geometries = null;
+		if(inDataStore!=null){
+			try {
+				SimpleFeatureSource inFeatureStore = (SimpleFeatureSource) inDataStore.getFeatureSource();
+				SimpleFeatureCollection inFeatureCollection = inFeatureStore.getFeatures();
+				geometries = new ArrayList<Geometry>();
+				SimpleFeatureIterator features = inFeatureCollection.features();
+				while(features.hasNext()){
+					SimpleFeature feature = features.next();
+					Geometry featureGeom = (Geometry) feature.getAttribute("the_geom");
+					// Expecting that shapefiles of polygons will contain MultiPolygon instances.
+					for(int i=0;i<featureGeom.getNumGeometries();i++){
+						Geometry geom = featureGeom.getGeometryN(i);
+						geometries.add(geom);
+					}			
+				}
+				features.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return geometries;
+	}
+
 }
